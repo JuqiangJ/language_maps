f83d8b11260492c584c0bd3f6f8431b1e59ba4e4
nikopartanen <nikotapiopartanen@gmail.com>
2016-03-07 21:34:00 +0100

    adding more stuff and the script to process shapefiles and geojson

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# language maps for different uses
+# Language maps for different uses
+
+This repository contains language maps in Shapefile and GeoJSON formats. Please notice that the actual files are always edited as Shapefiles, from which the GeoJSON is derived.
+
+CC-BY Niko Partanen 2016
---
+++ merge_shp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This is adapted from here:
+# http://www.northrivergeographic.com/ogr2ogr-merge-shapefiles
+# https://eomwandho.wordpress.com/tag/create-shapefile/
+
+for file in `find . -name "*.shp"`
+do
+    BASENAME=$(echo $file | sed 's/.shp//g')
+    ogr2ogr -f GeoJSON -t_srs crs:84 $BASENAME.geojson $file
+done
+
+file="language_maps.shp"
+
+for i in `find . -name "*.shp"`
+do
+
+    if [ -f "$file" ]
+    then
+	echo "creating merged shapefile"
+	ogr2ogr --config SHAPE_ENCODING UTF-8 -f 'ESRI Shapefile' -update -append $file $i -nln language_maps
+    else
+	echo "merging……"
+	ogr2ogr --config SHAPE_ENCODING UTF-8 -f 'ESRI Shapefile' $file $i
+fi
+done
