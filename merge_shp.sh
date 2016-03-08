b2ec153dd6f4de6ef661e70642f6f8fbc2289de1
nikopartanen <nikotapiopartanen@gmail.com>
2016-03-08 11:04:20 +0100

    realized that the file size somehow grew very big, now I found a better way to avoid whole part of merging everything into a new shapefile

--- merge_shp.sh
+++ merge_shp.sh
@@ -6,23 +6,28 @@
 
 for file in `find . -name "*.shp"`
 do
-    BASENAME=$(echo $file | sed 's/.shp//g')
-    ogr2ogr -f GeoJSON -t_srs crs:84 $BASENAME.geojson $file
+    BASENAME=$(echo $(basename $file) | sed 's/.shp//g')
+    ogr2ogr -f GeoJSON -t_srs crs:84 ./geojson/$BASENAME.geojson $file
 done
 
-file="language_maps.shp"
+# I found this tool from here
+# https://github.com/mapbox/geojson-merge
 
-for i in `find . -name "*.shp"`
-do
+geojson-merge geojson/*.geojson > language_map.geojson
 
-    if [ -f "$file" ]
-    then
-	echo "creating merged shapefile"
-	ogr2ogr --config SHAPE_ENCODING UTF-8 -f 'ESRI Shapefile' -update -append $file $i -nln language_maps
-    else
-	echo "merging……"
-	ogr2ogr --config SHAPE_ENCODING UTF-8 -f 'ESRI Shapefile' $file $i
-fi
-done
+# file="language_maps.shp"
 
-ogr2ogr -f GeoJSON -t_srs crs:84 language_maps.geojson language_maps.shp
+# for i in `find . -name "*.shp"`
+# do
+
+#    if [ -f "$file" ]
+#    then
+#	echo "creating merged shapefile"
+#	ogr2ogr --config SHAPE_ENCODING UTF-8 -f 'ESRI Shapefile' -update -append $file $i -nln language_maps
+#    else
+#	echo "merging……"
+#	ogr2ogr --config SHAPE_ENCODING UTF-8 -f 'ESRI Shapefile' $file $i
+# fi
+# done
+
+# ogr2ogr -f GeoJSON -t_srs crs:84 language_maps.geojson language_maps.shp
